c931ad9778c83c60d419727fbf4c976b2325281d
Joao Arquimedes <joaoarquimedes@gmail.com>
2017-01-31 09:41:11 -0200

    Update update_urlblacklist.sh

--- update_urlblacklist.sh
+++ update_urlblacklist.sh
@@ -12,51 +12,65 @@ URL="http://urlblacklist.com/cgi-bin/commercialdownload.pl?type=download&file=bi
 MD5="http://urlblacklist.com/cgi-bin/commercialdownload.pl?type=information&file=bigblacklist"
 
 FILE_SAVE="blacklist.tar.gz"
+DATESTAMP() { date +"%Y/%m/%d %H:%M:%S" ; }
+
+# Obtendo MD5 disponível pelo site
+url_md5=$(wget "$MD5" -q -O - | cut -d "," -f 2 | sed 's/\"//g')
+
 
 UPDATE() {
    wget "${URL}" -O "${FILE_SAVE}" > /dev/null 2> /dev/null
    if [[ $? -eq 0 ]]
    then
-      echo "--> Arquivo ${FILE_SAVE} salvo, descompactando."
+      file_md5=$(md5sum ${FILE_SAVE} | cut -d " " -f 1)
+      echo "$(DATESTAMP) --> MD5 do arquivo ${FILE_SAVE}: ${file_md5}"
+      if [[ "$url_md5" != "$file_md5" ]]
+      then
+         echo "$(DATESTAMP) --> Download do arquivo ${FILE_SAVE} incompleto, MD5 diferente. Saindo do script"
+         exit 1
+      fi
+
+      bDir="blacklists/"
+      echo "$(DATESTAMP) --> Limpando diretório $bDir"
+      mv $bDir /tmp/
+
+      echo "$(DATESTAMP) --> Arquivo ${FILE_SAVE} salvo, descompactando."
       tar -zxf $FILE_SAVE > /dev/null 2> /dev/null
       if [[ $? -eq 0 ]]
       then
-         echo "--> Arquivo descompactado, removendo download ${FILE_SAVE}"
+         echo "$(DATESTAMP) --> Arquivo descompactado, removendo download ${FILE_SAVE}"
          rm -r ${FILE_SAVE}
-         bDir="blacklists/"
-         echo "--> Ajustando permissões do diretório $bDir"
+         echo "$(DATESTAMP) --> Ajustando permissões do diretório $bDir"
          chown -R root. $bDir
          find $bDir -type d -print0 | xargs -0 chmod 775
          find $bDir -type f -print0 | xargs -0 chmod 664
       else
-         echo "--> Erro ao descompactar o arquivo ${FILE_SAVE}"
+         echo "$(DATESTAMP) --> Erro ao descompactar o arquivo ${FILE_SAVE}"
          rm -r "${FILE_SAVE}"
       fi
    else
-      echo "--> Erro ao realizar o download do arquivo ${FILE_SAVE}"
+      echo "$(DATESTAMP) --> Erro ao realizar o download do arquivo ${FILE_SAVE}"
       rm -r "${FILE_SAVE}"
    fi
 }
 
 CHECK_MD5() {
-   echo "--> Checando se houve atualização do arquivo ${FILE_SAVE}"
-   url_get=$(wget "$MD5" -q -O -)
-   url_md5=$(echo $url_get | cut -d "," -f 2 | sed 's/\"//g')
+   echo "$(DATESTAMP) --> Checando se houve atualização do arquivo ${FILE_SAVE}"
+
    loc_fil="local_md5.txt"
+   [[ ! -e $loc_fil ]] && touch $loc_fil
    loc_md5=$(cat $loc_fil)
 
-   [[ -e $loc_md5 ]] || touch $loc_fil
-
-   echo "--> MD5 da URL: $url_md5"
-   echo "--> MD5 do arquivo local: $loc_md5"
+   echo "$(DATESTAMP) --> MD5 da URL: $url_md5"
+   echo "$(DATESTAMP) --> MD5 do arquivo local: $loc_md5"
    
    if [[ "$url_md5" != "$loc_md5" ]]
    then
-      echo "--> MD5 Diferente, atualizando arquivos"
+      echo "$(DATESTAMP) --> MD5 Diferente, atualizando arquivos"
       echo "$url_md5" > $loc_fil
       UPDATE
    else
-      echo "--> MD5 igual, não há necessidade de atualização"
+      echo "$(DATESTAMP) --> MD5 igual, não há necessidade de atualização"
       exit 0
    fi
 }
